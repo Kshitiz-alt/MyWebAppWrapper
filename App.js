8ee14017c4b621b0af86179051f9bce484738e28
Kshitiz-alt <saxenaktz@gmail.com>
2025-07-13 12:09:17 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {WebView} from 'react-native-webview'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="dark-content" />
+      <WebView 
+        source={{ uri: 'https://my-app-alpha-weld.vercel.app' }}
+        style={{ flex: 1 }}
+        startInLoadingState={true}
+      />
     </View>
   );
 }
@@ -13,8 +17,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
